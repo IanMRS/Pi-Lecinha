d8eec0a4fc34c2494ae3791844fddefa3494ab7d
Zer0-Zer0 <dankmemerson@tutanota.com>
2023-11-07 18:00:41 -0300

    added proper documentation to the CRUD class

--- db_action.py
+++ db_action.py
@@ -4,13 +4,60 @@ from tkinter import ttk
 import db_connection as dbc
 
 class CRUD:
-    def __init__(self, table_name, columns):
+    """
+    A class for performing CRUD (Create, Read, Update, Delete) operations on a database table.
+
+    Args:
+        table_name (str): The name of the database table to interact with.
+        columns (list): A list of column names in the table.
+        
+    Attributes:
+        table_name (str): The name of the database table.
+        columns (list): List of column names.
+        conn: A database connection object.
+        cursor: A cursor object for executing SQL commands.
+
+    Methods:
+        - insert(data)
+        - read()
+        - update(data, condition)
+        - delete(condition)
+        - close_connection()
+
+    Example usage:
+        my_crud = CRUD("employees", ["id", "name", "salary"])
+    """
+
+
+    def __init__(self, table_name : str, columns : list):
+        """
+        Initializes a CRUD object for a specific database table.
+
+        Args:
+            table_name (str): The name of the database table to interact with.
+            columns (list): A list of column names in the table.
+        """
+
         self.table_name = table_name
         self.columns = columns
         self.conn = dbc.connect_db()
         self.cursor = self.conn.cursor()
 
+
     def insert(self, data):
+        """
+        Insert data into the database table.
+
+        Args:
+            data (tuple): A tuple containing values to be inserted into the table, in the same order as columns.
+
+        Raises:
+            Exception: If the database connection is not established.
+
+        Example usage:
+            my_crud.insert((1, "John Doe", 50000))
+        """
+
         if self.conn is None or self.cursor is None:
             raise Exception("Connection to the database is not established. Call connect_to_database() first.")
         placeholders = ', '.join(['?'] * len(data))
@@ -18,28 +65,79 @@ class CRUD:
         self.cursor.execute(insert_query, data)
         self.conn.commit()
 
+
     def read(self):
+        """
+        Retrieve all records from the database table.
+
+        Returns:
+            list: A list of tuples, each representing a row in the table.
+
+        Raises:
+            Exception: If the database connection is not established.
+
+        Example usage:
+            data = my_crud.read()
+        """
+
         if self.conn is None or self.cursor is None:
             raise Exception("Connection to the database is not established. Call connect_to_database() first.")
         select_query = f"SELECT * FROM {self.table_name}"
         self.cursor.execute(select_query)
         return self.cursor.fetchall()
 
+
     def update(self, data, condition):
+        """
+        Update records in the database table based on a condition.
+
+        Args:
+            data (str): A string representing the columns and values to update, e.g., "name = 'New Name'".
+            condition (str): A string representing the condition for updating records, e.g., "id = 1".
+
+        Raises:
+            Exception: If the database connection is not established.
+
+        Example usage:
+            my_crud.update("name = 'Updated Name'", "id = 1")
+        """
+
         if self.conn is None or self.cursor is None:
             raise Exception("Connection to the database is not established. Call connect_to_database() first.")
         update_query = f"UPDATE {self.table_name} SET {data} WHERE {condition}"
         self.cursor.execute(update_query)
         self.conn.commit()
 
+
     def delete(self, condition):
+        """
+        Delete records from the database table based on a condition.
+
+        Args:
+            condition (str): A string representing the condition for deleting records, e.g., "id = 1".
+
+        Raises:
+            Exception: If the database connection is not established.
+
+        Example usage:
+            my_crud.delete("id = 1")
+        """
+
         if self.conn is None or self.cursor is None:
             raise Exception("Connection to the database is not established. Call connect_to_database() first.")
         delete_query = f"DELETE FROM {self.table_name} WHERE {condition}"
         self.cursor.execute(delete_query)
         self.conn.commit()
 
+
     def close_connection(self):
+        """
+        Close the database connection if it is open.
+
+        Example usage:
+            my_crud.close_connection()
+        """
+
         if self.conn:
             self.conn.close()
 
