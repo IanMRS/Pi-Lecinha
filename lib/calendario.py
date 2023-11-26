48c6834a29304854d6e8ade774978268461c9d07
Zer0-Zer0 <dankmemerson@tutanota.com>
2023-11-26 19:24:17 -0300

    resolvido bug de display

--- lib/calendario.py
+++ lib/calendario.py
@@ -4,35 +4,33 @@ import calendar
 from lib import crud as c
 
 class Calendario(Frame):
-    def __init__(self,frame):
+    def __init__(self, frame):
         super().__init__(frame)
 
         self.top_row = Frame(self)
-        self.top_row.pack(padx=10, pady=10)
+        self.top_row.grid(row=0, column=0)
 
         self.frame = Frame(self)
-        self.frame.pack(padx=10, pady=10)
+        self.frame.grid(row=1, column=0)
 
         self.current_date = calendar.datetime.datetime.now()
 
         self.prev_month_button = Button(self.top_row, text="Previous Month", command=self.show_previous_month)
         self.prev_month_button.grid(row=0, column=0)
 
-
         self.current_month = Label(self.top_row, text=self.num_to_month(self.current_date.month))
-        self.current_month.grid(row=0, column=4)
-
+        self.current_month.grid(row=0, column=1, padx=10)  # Adjusted column and added padx for spacing
 
         self.next_month_button = Button(self.top_row, text="Next Month", command=self.show_next_month)
-        self.next_month_button.grid(row=0, column=8)
+        self.next_month_button.grid(row=0, column=2)  # Adjusted column
 
         self.days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
         for i, day in enumerate(self.days):
-            day_label = Label(frame, text=day)
-            day_label.grid(row=1, column=i+1)
+            day_label = Label(self.frame, text=day)
+            day_label.grid(row=0, column=i+1)
 
-        cal_display = Label(frame, text="", justify='left')
-        cal_display.grid(row=3, columnspan=8)
+        self.cal_display = Label(self.frame, text="", justify='left')  # Changed variable name to cal_display
+        self.cal_display.grid(row=1, column=0, columnspan=8)  # Adjusted row and columnspan
 
         self.day_buttons = []  # Store day buttons
 
@@ -73,23 +71,23 @@ class Calendario(Frame):
 
 
     def create_day_buttons(self, year, month):
-        # Configure uniform button size
-        button_width = 5
-        button_height = 2
+            # Configure uniform button size
+            button_width = 5
+            button_height = 2
 
-        for button in self.day_buttons:
-            button.grid_forget()  # Clear existing buttons
+            for button in self.day_buttons:
+                button.grid_forget()  # Clear existing buttons
 
-        _, last_day = calendar.monthrange(year, month)
-        first_weekday, _ = calendar.monthrange(year, month)
+            _, last_day = calendar.monthrange(year, month)
+            first_weekday, _ = calendar.monthrange(year, month)
 
-        for i in range(1, last_day + 1):
-            day_button = Button(self, text=str(i), command=lambda i=i: self.show_day(i), width=button_width, height=button_height,borderwidth=2, relief="ridge")
-            row = 4 + (i + first_weekday - 2) // 7
-            col = 1+ (i + first_weekday - 2) % 7
+            for i in range(1, last_day + 1):
+                day_button = Button(self.frame, text=str(i), command=lambda i=i: self.show_day(i), width=button_width, height=button_height, borderwidth=2, relief="ridge")
+                row = 2 + (i + first_weekday - 2) // 7  # Adjusted row
+                col = (i + first_weekday - 2) % 7 + 1
 
-            day_button.grid(row=row, column=col)
-            self.day_buttons.append(day_button)
+                day_button.grid(row=row, column=col)
+                self.day_buttons.append(day_button)
 
 
     def num_to_month(self, num):
