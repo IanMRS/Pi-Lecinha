b8de19735a8100fa7af9b45ae61a7fecefe53157
Zer0-Zer0 <dankmemerson@tutanota.com>
2023-11-27 15:55:32 -0300

    removed redundancies

--- lib/screen.py
+++ lib/screen.py
@@ -7,32 +7,31 @@ from lib import crud
 from lib.calendario import Calendario
 from lib.dashboard import FinanceiroApp
 
-class MainScreen(w.Window):
+class MainScreen(Window):
     def __init__(self):
-        self.main_window=Tk()
-        self.frames_da_tela()
-        self.menus()
+        super().__init__()
 
-        self.main_window.title(f"Administrar aluguel de temporada")
-        self.main_window.configure(background='#444444')
-        self.main_window.resizable(False, False)
+        self.frames_da_tela()
+
+        self.root.title(f"Administrar aluguel de temporada")
+        self.root.configure(background='#444444')
         
     
     def frames_da_tela(self):
-        self.notebook = ttk.Notebook(self.main_window)  # Criar o Notebook
+        self.notebook = ttk.Notebook(self.root)  # Criar o Notebook
 
-        abas = [('Dashboard Financeiro',    d.FinanceiroApp(self.notebook)),
-                ('Clientes',                man.GenericManager(crud.bancos["cliente"],self.notebook)),
-                ('Casas',                   man.GenericManager(crud.bancos["casa"],self.notebook)),
-                ('Sites',                   man.GenericManager(crud.bancos["origem"],self.notebook)),
-                ('Alugueis',                man.GenericManager(crud.bancos["aluguel"],self.notebook)),
-                ('Aluguel-Casa',            man.GenericManager(crud.bancos["aluguel_has_casa"],self.notebook)),
-                ('Calendário',              car.Calendario(self.notebook))]
+        abas = [('Dashboard Financeiro',    FinanceiroApp(self.notebook)),
+                ('Clientes',                GenericManager(crud.bancos["cliente"],self.notebook)),
+                ('Casas',                   GenericManager(crud.bancos["casa"],self.notebook)),
+                ('Sites',                   GenericManager(crud.bancos["origem"],self.notebook)),
+                ('Alugueis',                GenericManager(crud.bancos["aluguel"],self.notebook)),
+                ('Aluguel-Casa',            GenericManager(crud.bancos["aluguel_has_casa"],self.notebook)),
+                ('Calendário',              Calendario(self.notebook))]
             
-        for titulo, aba in abas:
-            self.notebook.add(aba, text = titulo)
+        for titulo, aba in abas:                        # Adicionar as abas
+            self.notebook.add(aba, text = titulo)       
 
-        self.notebook.pack(fill='both', expand=True)  # Empacotar o Notebook para exibição
+        self.notebook.pack(fill='both', expand=True)    # Empacotar o Notebook para exibição
 
 tela = MainScreen()
 tela.start()
