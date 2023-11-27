491ed5d39446728deab7ecfcbf93809f9d04272e
Zer0-Zer0 <dankmemerson@tutanota.com>
2023-11-27 16:15:38 -0300

    whitespace

--- lib/window.py
+++ lib/window.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 
+
 class FullScreenWindow:
     def __init__(self, title=None):
         self.root = tk.Tk()
@@ -7,15 +8,19 @@ class FullScreenWindow:
 
         self.setup_key_bindings()
 
+
     def setup_key_bindings(self):
         self.root.bind('<F11>', self.toggle_full_screen)
         self.root.bind('<Escape>', self.exit_full_screen)
 
+
     def start(self):
         self.root.mainloop()
 
+
     def stop(self):
         self.root.destroy()
+
 
     def toggle_full_screen(self, event=None):
         """Toggle between fullscreen and normal mode."""
@@ -23,6 +28,7 @@ class FullScreenWindow:
             self.root.attributes('-fullscreen', not self.root.attributes('-fullscreen'))
         except tk.TclError as e:
             print(f"Error toggling fullscreen: {e}")
+
 
     def exit_full_screen(self, event=None):
         """Exit fullscreen mode."""
