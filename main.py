3813b8a39d7670ed648b6119189f532245c9846f
Zer0-Zer0 <dankmemerson@tutanota.com>
2023-11-27 14:10:15 -0300

    Revomed unnecessary import

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from lib import connection as dbc
 from lib import screen as s
 
-dbc.create_db() # Cria um banco de dados local
+#dbc.create_db() # Cria um banco de dados local
 
 s.tela.start() # Cria uma instância da classe Application, o que inicia a aplicação TKINTER
