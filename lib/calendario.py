9dacfbf05c717f548a16a6cbd579211bc67a7018
Zer0-Zer0 <dankmemerson@tutanota.com>
2023-11-26 21:16:56 -0300

    day coloring logic

--- lib/calendario.py
+++ lib/calendario.py
@@ -7,29 +7,29 @@ class Calendario(Frame):
     def __init__(self, frame):
         super().__init__(frame)
 
-        self.top_row = Frame(self)
-        self.top_row.grid(row=0, column=0)
+        self.header_frame = Frame(self)
+        self.header_frame.grid(row=0, column=0)
 
-        self.frame = Frame(self)
-        self.frame.grid(row=1, column=0)
+        self.calendar_frame = Frame(self)
+        self.calendar_frame.grid(row=1, column=0)
 
         self.current_date = calendar.datetime.datetime.now()
 
-        self.prev_month_button = Button(self.top_row, text="Previous Month", command=self.show_previous_month)
+        self.prev_month_button = Button(self.header_frame, text="Previous Month", command=self.show_previous_month)
         self.prev_month_button.grid(row=0, column=0)
 
-        self.current_month = Label(self.top_row, text=self.num_to_month(self.current_date.month))
+        self.current_month = Label(self.header_frame, text=self.num_to_month(self.current_date.month))
         self.current_month.grid(row=0, column=1, padx=10)  # Adjusted column and added padx for spacing
 
-        self.next_month_button = Button(self.top_row, text="Next Month", command=self.show_next_month)
+        self.next_month_button = Button(self.header_frame, text="Next Month", command=self.show_next_month)
         self.next_month_button.grid(row=0, column=2)  # Adjusted column
 
         self.days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
         for i, day in enumerate(self.days):
-            day_label = Label(self.frame, text=day)
+            day_label = Label(self.calendar_frame, text=day)
             day_label.grid(row=0, column=i+1)
 
-        self.cal_display = Label(self.frame, text="", justify='left')  # Changed variable name to cal_display
+        self.cal_display = Label(self.calendar_frame, text="", justify='left')  # Changed variable name to cal_display
         self.cal_display.grid(row=1, column=0, columnspan=8)  # Adjusted row and columnspan
 
         self.day_buttons = []  # Store day buttons
@@ -37,8 +37,19 @@ class Calendario(Frame):
         self.show_month()
 
 
+    def ler_alugueis(self):
+        self.dados_aluguel = c.crud_aluguel.read()
+        self.dados_casa = c.crud_casa.read()
+
+
+    def formatted_date(self):
+        return f"{self.current_date.year}{self.current_date.month}"
+
+
     def show_month(self):
         year, month = self.current_date.year, self.current_date.month
+
+        self.ler_alugueis()
         self.create_day_buttons(year, month)
         
         self.current_month.config(text=self.num_to_month(month))
@@ -75,6 +86,10 @@ class Calendario(Frame):
             button_width = 5
             button_height = 2
 
+
+            dates_in_month = []
+
+
             for button in self.day_buttons:
                 button.grid_forget()  # Clear existing buttons
 
@@ -82,13 +97,35 @@ class Calendario(Frame):
             first_weekday, _ = calendar.monthrange(year, month)
 
             for i in range(1, last_day + 1):
-                day_button = Button(self.frame, text=str(i), command=lambda i=i: self.show_day(i), width=button_width, height=button_height, borderwidth=2, relief="ridge")
+                day_button = Button(self.calendar_frame, text=str(i), command=lambda i=i: self.show_day(i), width=button_width, height=button_height, borderwidth=2, relief="ridge")
+
+                for data in self.dados_aluguel:
+                    starts_in_this_month = str(data[3])[:6] == self.formatted_date()
+                    starts_in_other_month = str(data[3])[:6] != self.formatted_date()
+
+                    day_is_in_it = int(str(data[3])[6:]) <= i
+                    print(f"{int(str(data[3])[6:])} , {i}")
+
+
+                    ends_in_other_month = str(data[4])[:6] != self.formatted_date() and int(str(data[4])[6:]) <= i
+                    ends_in_this_month = str(data[4])[:6] == self.formatted_date() and int(str(data[4])[6:]) >= i
+
+                    #print(int(str(data[4])[6:]))
+                    
+                    if starts_in_other_month and ends_in_this_month:
+                        day_button.configure(bg='red')  # Change the color to red (you can modify this)
+
+                    if  starts_in_this_month and day_is_in_it and ends_in_this_month:
+                        day_button.configure(bg='blue')  # Change the color to red (you can modify this)
+
+                    if  starts_in_this_month and day_is_in_it and ends_in_other_month:
+                        day_button.configure(bg='green')
+
                 row = 2 + (i + first_weekday - 2) // 7  # Adjusted row
                 col = (i + first_weekday - 2) % 7 + 1
 
                 day_button.grid(row=row, column=col)
                 self.day_buttons.append(day_button)
-
 
     def num_to_month(self, num):
         months = {
