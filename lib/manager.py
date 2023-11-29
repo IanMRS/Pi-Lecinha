c1574ae0bc6845540c4e5cec927bf6463a8901b2
Zer0-Zer0 <dankmemerson@tutanota.com>
2023-11-29 16:34:52 -0300

    translated all names to english

--- lib/manager.py
+++ lib/manager.py
@@ -14,14 +14,14 @@ class GenericManager(Frame):
     def __init__(self, crud, frame):
         super().__init__(frame)
         self.crud = crud
-        self.nome_dado = self.crud.table_name.capitalize()
+        self.data_name = self.crud.table_name.capitalize()
 
         self.configure(background=BACKGROUND_COLOR)
 
         self.frames()
         self.widgets()
         self.table()
-        self.update_lista()
+        self.update_list()
 
     def frames(self):
         frame_names = ['top_row', 'inputs', 'bottom_row']
@@ -45,72 +45,72 @@ class GenericManager(Frame):
             entry.grid(row=1, column=i)
             self.entries.append(entry)
 
-        self.botoes = [
+        self.buttons = [
             ("Novo", self.insert_button_pressed),
             ("Alterar", self.update_button_pressed),
             ("Buscar", self.search_button_pressed),
-            ("Limpar", self.limpa_tela),
+            ("Limpar", self.clear_screen),
             ("Apagar", self.delete_button_pressed)
         ]
 
-        for i, (text, command) in enumerate(self.botoes):
+        for i, (text, command) in enumerate(self.buttons):
             Button(self.top_row, text=text, command=command, width=BUTTON_WIDTH, height=BUTTON_HEIGHT, relief=BUTTON_RELIEF).grid(row=0, column=i)
 
     def table(self):
         table_columns = self.crud.columns
 
-        self.lista_itens = ttk.Treeview(self.bottom_row, columns=table_columns, show='headings')
+        self.item_table = ttk.Treeview(self.bottom_row, columns=table_columns, show='headings')
         for column in table_columns:
-            self.lista_itens.heading(column, text=column.capitalize())
-            self.lista_itens.column(column, anchor='center')
+            self.item_table.heading(column, text=column.capitalize())
+            self.item_table.column(column, anchor='center')
 
         for i in range(len(table_columns)):
-            self.lista_itens.column(i, stretch=YES)
+            self.item_table.column(i, stretch=YES)
 
-        self.lista_itens.grid(row=0, column=0, sticky="nsew")
+        self.item_table.grid(row=0, column=0, sticky="nsew")
 
-        scroll_lista = Scrollbar(self.bottom_row, orient=VERTICAL, command=self.lista_itens.yview)
-        self.lista_itens.configure(yscroll=scroll_lista.set)
-        scroll_lista.grid(row=0, column=1, sticky="ns")
-        self.lista_itens.bind("<Double-1>", self.double_click)
+        scroll_list = Scrollbar(self.bottom_row, orient=VERTICAL, command=self.item_table.yview)
+        self.item_table.configure(yscroll=scroll_list.set)
+        scroll_list.grid(row=0, column=1, sticky="ns")
+        self.item_table.bind("<Double-1>", self.double_click)
 
     def insert_button_pressed(self):
         self.crud.insert(self.get_entries_content())
-        self.update_lista()
-        self.limpa_tela()
+        self.update_list()
+        self.clear_screen()
 
     def update_button_pressed(self):
         self.crud.update(self.get_entries_content(), f"id = {self.get_entries_content()[0]}")
-        self.update_lista()
-        self.limpa_tela()
+        self.update_list()
+        self.clear_screen()
 
     def search_button_pressed(self):
         self.crud.search(self.get_entries_content())
-        self.update_lista()
+        self.update_list()
 
     def delete_button_pressed(self):
         self.crud.delete(f"id = {self.get_entries_content()[0]}")
-        self.update_lista()
-        self.limpa_tela()
+        self.update_list()
+        self.clear_screen()
 
-    def limpa_tela(self):
+    def clear_screen(self):
         for entry in self.entries:
             entry.delete(0, END)
 
-    def update_lista(self, lista=None):
-        self.lista_itens.delete(*self.lista_itens.get_children())
-        if lista is None:
-            lista = self.crud.read()
-        for item in lista:
-            temp_lista = [GenericManager.unformat_date(element) if "data" in self.crud.columns[index] else element for index, element in enumerate(item)]
-            self.lista_itens.insert("", END, values=temp_lista)
+    def update_list(self, selected_list=None):
+        self.item_table.delete(*self.item_table.get_children())
+        if selected_list is None:
+            selected_list = self.crud.read()
+        for item in selected_list:
+            temp_list = [GenericManager.unformat_date(element) if "data" in self.crud.columns[index] else element for index, element in enumerate(item)]
+            self.item_table.insert("", END, values=temp_list)
 
     def double_click(self, event):
-        selected_row = self.lista_itens.selection()
+        selected_row = self.item_table.selection()
         if selected_row:
-            self.limpa_tela()
+            self.clear_screen()
             for row_id in selected_row:
-                columns = self.lista_itens.item(row_id, 'values')
+                columns = self.item_table.item(row_id, 'values')
                 [entry.insert(END, col) for col, entry in zip(columns, self.entries)]
 
     @staticmethod
