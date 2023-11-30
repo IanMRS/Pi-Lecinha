47c1fa5935ea3052076040820f4b4b4f9aad1bc0
Zer0-Zer0 <dankmemerson@tutanota.com>
2023-11-30 16:37:52 -0300

    organizing stuff around

--- lib/manager.py
+++ lib/manager.py
@@ -35,6 +35,7 @@ class GenericManager(Frame):
             "<Delete>": self.delete_button_pressed,
             "<Shift-BackSpace>": self.delete_button_pressed,
             "<Escape>": self.unselect_inputs,
+            "<Double-1>": self.on_double_click
         }
 
         for key, command in shortcut_mapping.items():
@@ -44,7 +45,7 @@ class GenericManager(Frame):
         self.refresh_inputs()
 
     def stop_keybinds(self, event=None):
-        for key in ["<Return>", "<Control-Return>", "<Control-BackSpace>", "<Shift-BackSpace>", "<Delete>", "<Escape>"]:
+        for key in ["<Return>", "<Control-Return>", "<Control-BackSpace>", "<Shift-BackSpace>", "<Delete>", "<Escape>","<Double-1>"]:
             self.winfo_toplevel().unbind(key)
 
     def init_frames(self):
@@ -114,41 +115,35 @@ class GenericManager(Frame):
         label.grid(row=0, column=i)
         entry.grid(row=1, column=i)
 
-    def refresh_inputs(self):
-        for index, entry in enumerate(self.entries):
-            if isinstance(entry, ttk.Combobox):
-                self.update_combobox_values(entry, index)
-
-    def update_combobox_values(self, entry, index):
-        banco_anterior = self.crud.table_name
-        temp_banco = self.crud.columns[index][3:]
-        temp_search = c.BANCOS[temp_banco].read()
-
-        while "id_" in c.BANCOS[temp_banco].columns[1]:
-            banco_anterior = temp_banco
-            temp_banco = c.BANCOS[temp_banco].columns[1][3:]
-            temp_search = c.BANCOS[temp_banco].db_input(
-                f"SELECT c.* FROM {temp_banco} c JOIN {banco_anterior} a ON c.id = a.id_{temp_banco};").fetchall()
-
-        options = [element[1] for element in temp_search]
-        entry["values"] = options
-
     def create_table_heading(self, column):
-        column_text = column[3:].capitalize(
-        ) if "id_" in column[:3] else column.capitalize()
+        column_text = column[3:].capitalize() if "id_" in column[:3] else column.capitalize()
         self.item_table.heading(column, text=column_text)
-
-    def configure_table_column(self, column):
-        self.item_table.column(column, anchor="center")
 
     def create_table_and_scroll_list(self):
         self.item_table.grid(row=0, column=0, sticky="nsew")
 
-        scroll_list = Scrollbar(
-            self.bottom_row, orient=VERTICAL, command=self.item_table.yview)
+        scroll_list = Scrollbar(self.bottom_row, orient=VERTICAL, command=self.item_table.yview)
         self.item_table.configure(yscroll=scroll_list.set)
         scroll_list.grid(row=0, column=1, sticky="ns")
-        self.item_table.bind("<Double-1>", self.double_click)
+
+    def configure_table_column(self, column):
+        self.item_table.column(column, anchor="center")
+
+    def on_enter(self, event=None):
+        (self.update_button_pressed if "" not in self.get_inputs_content()
+         else self.search_button_pressed)()
+
+    def on_control_enter(self, event=None):
+        (self.insert_button_pressed if self.get_inputs_content()[0] == ""
+         else self.update_button_pressed)()
+
+    def on_double_click(self, event=None):
+        selected_row = self.item_table.selection()
+        if selected_row:
+            self.clear_inputs()
+            for row_id in selected_row:
+                columns = self.item_table.item(row_id, "values")
+                self.insert_values_in_entries(columns)
 
     def insert_button_pressed(self, event=None):
         self.crud.insert(self.get_inputs_content())
@@ -158,14 +153,6 @@ class GenericManager(Frame):
         condition = f"id = {self.get_inputs_content()[0]}"
         self.crud.update(self.get_inputs_content(), condition)
         self.clear_button_pressed()
-
-    def on_enter(self, event=None):
-        (self.update_button_pressed if "" not in self.get_inputs_content()
-         else self.search_button_pressed)()
-
-    def on_control_enter(self, event=None):
-        (self.insert_button_pressed if self.get_inputs_content()[0] == ""
-         else self.update_button_pressed)()
 
     def search_button_pressed(self, event=None):
         search_result = self.crud.search(self.get_inputs_content())
@@ -211,6 +198,24 @@ class GenericManager(Frame):
     def unselect_inputs(self, event=None):
         self.focus_set()
 
+    def refresh_inputs(self):
+        for index, entry in enumerate(self.entries):
+            if isinstance(entry, ttk.Combobox):
+                self.refresh_dropdowns(entry, index)
+
+    def refresh_dropdowns(self, entry, index):
+        banco_anterior = self.crud.table_name
+        temp_banco = self.crud.columns[index][3:]
+        temp_search = c.BANCOS[temp_banco].read()
+
+        while "id_" in c.BANCOS[temp_banco].columns[1]:
+            banco_anterior = temp_banco
+            temp_banco = c.BANCOS[temp_banco].columns[1][3:]
+            temp_search = c.BANCOS[temp_banco].db_input(f"SELECT c.* FROM {temp_banco} c JOIN {banco_anterior} a ON c.id = a.id_{temp_banco};").fetchall()
+
+        options = [element[1] for element in temp_search]
+        entry["values"] = options
+
     def refresh_table(self, table_values = None):
         table_values = self.crud.read() if table_values is None else table_values
         self.item_table.delete(*self.item_table.get_children())
@@ -239,25 +244,6 @@ class GenericManager(Frame):
                     temp_list.append(element)
             self.item_table.insert("", END, values=temp_list)
 
-    def double_click(self, event=None):
-        selected_row = self.item_table.selection()
-        if selected_row:
-            self.clear_inputs()
-            for row_id in selected_row:
-                columns = self.item_table.item(row_id, "values")
-                self.insert_values_in_entries(columns)
-
-    @staticmethod
-    def format_date(selected_date):
-        if isinstance(selected_date, str):
-            return selected_date
-        return datetime.strftime(selected_date, "%Y%m%d")
-
-    @staticmethod
-    def unformat_date(selected_date):
-        parsed_date = datetime.strptime(str(selected_date), "%Y%m%d")
-        return parsed_date.strftime("%d/%m/%y")
-
     def get_inputs_content(self):
         inputs = []
         for entry in self.entries:
@@ -269,3 +255,14 @@ class GenericManager(Frame):
                 inputs.append(entry.get())
 
         return inputs
+
+    @staticmethod
+    def format_date(selected_date):
+        if isinstance(selected_date, str):
+            return selected_date
+        return datetime.strftime(selected_date, "%Y%m%d")
+
+    @staticmethod
+    def unformat_date(selected_date):
+        parsed_date = datetime.strptime(str(selected_date), "%Y%m%d")
+        return parsed_date.strftime("%d/%m/%y")
