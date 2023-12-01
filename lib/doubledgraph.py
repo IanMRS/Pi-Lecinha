fddb8e933cad663ad693eb8c524a3d62510ee1a9
Zer0-Zer0 <dankmemerson@tutanota.com>
2023-12-01 16:27:33 -0300

    removidos tabs duplos

--- lib/doubledgraph.py
+++ lib/doubledgraph.py
@@ -3,10 +3,10 @@ from lib.calendario import GUICalendar
 from tkinter import *
 
 class DoubleGraph(Frame):
-          def __init__(self,frame):
-                    super().__init__(frame)
-                    self.dashboard = FinanceiroApp(self)
-                    self.calendario = GUICalendar(self)
-                    self.dashboard.grid(row=0, column=0)
-                    self.calendario.grid(row=0, column=1)
+    def __init__(self,frame):
+        super().__init__(frame)
+        self.dashboard = FinanceiroApp(self)
+        self.calendario = GUICalendar(self)
+        self.dashboard.grid(row=0, column=0)
+        self.calendario.grid(row=0, column=1)
                     
