0f2940b0ec98ccfe4f34d8ffbcaa0ba52e7d909d
Zer0-Zer0 <dankmemerson@tutanota.com>
2023-12-01 16:28:43 -0300

    added prints

--- lib/manager.py
+++ lib/manager.py
@@ -3,17 +3,17 @@ from lib.manager_table import ManagerTable
 from lib.manager_inputs import ManagerInputs
 from lib.manager_buttons import ManagerButtons
 
-PADDING_X = 10
-PADDING_Y = 10
-BACKGROUND_COLOR = "#444444"
-
 class GenericManager(Frame):
+    PADDING_X = 10
+    PADDING_Y = 10
+    BACKGROUND_COLOR = "#444444"
+    
     def __init__(self, crud, frame = None):
-        print(f"\nAdmin. {self.data_name}: Inicializando")
-        super().__init__(frame)
         self.crud = crud
         self.data_name = self.crud.table_name.capitalize()
+        print(f"\nAdmin. {self.data_name}: Inicializando")
 
+        super().__init__(frame)
         self.configure_widgets()
         self.pack_widgets()
 
@@ -21,6 +21,7 @@ class GenericManager(Frame):
         self.bind("<FocusOut>", self.buttons.stop_keybinds)
 
     def configure_widgets(self):
+        print(f"Admin. {self.data_name}: Configurando Widgets")
         self.configure(background=BACKGROUND_COLOR)
 
         self.inputs = ManagerInputs(self, self.crud)
@@ -28,6 +29,7 @@ class GenericManager(Frame):
         self.buttons = ManagerButtons(self, self.crud, self.inputs, self.table)
 
     def pack_widgets(self):
+        print(f"Admin. {self.data_name}: Empacotando Widgets")
         self.buttons.pack(padx=PADDING_X, pady=PADDING_Y)
         self.inputs.pack(padx=PADDING_X, pady=PADDING_Y)
         self.table.pack(fill="both", expand=True, padx=PADDING_X, pady=PADDING_Y)
